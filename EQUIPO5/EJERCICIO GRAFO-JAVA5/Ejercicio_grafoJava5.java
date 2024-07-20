1408443b562896a384f347502c1bf5a00ee2f0b7
Ilderios <ilderios030405@micorreo.upp.edu.mx>
2024-07-20 11:05:15 -0600

    Update Ejercicio_grafoJava5.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Ejercicio_grafoJava5.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Ejercicio_grafoJava5.java
@@ -1,15 +1,14 @@
 public class Ejercicio_grafoJava5 {
 
+
+
+
+
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.err.println("Holaaa");
-        System.err.println("SI COMPILA");
-        System.err.println("Pruebas de codigo");
-
-        System.err.println("Hola Mundo");
-        System.err.println("ilderio1");
     }
 }
