101f5640c66324264b2ec6f651b941742c4349e7
Ilderios <ilderios030405@micorreo.upp.edu.mx>
2024-07-20 13:13:37 -0600

    Create Grafo.java

---
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Grafo.java
@@ -0,0 +1,24 @@
+public class Grafo{
+
+    private Vertice inicio;
+
+    public Grafo(){
+        this.inicio = null;
+    }
+
+    public Vertice getInicio(){
+        return inicio;
+    }
+
+    public void setInicio(Vertice inicio){
+        this.inicio = inicio;
+    }
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
