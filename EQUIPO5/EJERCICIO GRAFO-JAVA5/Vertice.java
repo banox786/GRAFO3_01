aaca3a1c87a6312ddb9fcf78d6f58f320743e292
Ilderios <ilderios030405@micorreo.upp.edu.mx>
2024-07-20 13:13:42 -0600

    Update Vertice.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
@@ -1,3 +1,32 @@
 public class Vertice {
-    
+    private int dato;
+    private Vertice sigVertice;
+    private Arista inicioArista;
+
+    public Vertice(int dato){
+        this.dato = dato;
+        this.sigVertice = null;
+        this.inicioArista = null;
+    }
+
+    public int getDato(){
+        return dato;
+    }
+    public void setDato(int dato){
+        this.dato = dato;
+    }
+
+    public Vertice getSigVertice(){
+        return sigVertice;
+    }
+    public void setSigVertice(Vertice sigVertice){
+        this.sigVertice = sigVertice;
+    }
+
+    public Arista getInicioArista(){
+        return inicioArista;
+    }
+    public void setInicioArista(Arista inicioArista){
+        this.inicioArista = inicioArista;
+    }
 }
