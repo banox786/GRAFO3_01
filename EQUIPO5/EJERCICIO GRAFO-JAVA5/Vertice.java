aa3599c080df38fd7972ebd47fa049a500c13e3b
IRojit0 <159846136+AleMaPa0@users.noreply.github.com>
2024-07-21 14:08:35 -0600

    Update Vertice.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
@@ -1,11 +1,27 @@
 public class Vertice {
+<<<<<<< HEAD
     private String capacidad;
+=======
+<<<<<<< HEAD
+    private String capacidad;
+=======
+    private int capacidad;
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
+>>>>>>> 7f3e106a152c4ef7d62e4b527bdbf6f8fb715176
     private String tipo;
     private String ubicacion;
     private Vertice sigVertice;
     private Arista inicioArista;
 
+<<<<<<< HEAD
     public Vertice(String capacidad, String tipo, String ubicacion){
+=======
+<<<<<<< HEAD
+    public Vertice(String capacidad, String tipo, String ubicacion) {
+=======
+    public Vertice(int capacidad, String tipo, String ubicacion){
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
+>>>>>>> 7f3e106a152c4ef7d62e4b527bdbf6f8fb715176
         this.capacidad = capacidad;
         this.tipo = tipo;
         this.ubicacion = ubicacion;
@@ -13,8 +29,37 @@ public class Vertice {
         this.inicioArista = null;
     }
 
+<<<<<<< HEAD
+    public String getCapacidad() {
+=======
+<<<<<<< HEAD
     public String getCapacidad() {
         return capacidad;
+=======
+    public int getCapacidad() {
+>>>>>>> 7f3e106a152c4ef7d62e4b527bdbf6f8fb715176
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
     }
 
     public void setCapacidad(String capacidad) {
@@ -37,17 +82,24 @@ public class Vertice {
         this.ubicacion = ubicacion;
     }
 
-    public Vertice getSigVertice(){
+    public Vertice getSigVertice() {
         return sigVertice;
     }
-    public void setSigVertice(Vertice sigVertice){
+
+    public void setSigVertice(Vertice sigVertice) {
         this.sigVertice = sigVertice;
     }
 
-    public Arista getInicioArista(){
+    public Arista getInicioArista() {
         return inicioArista;
     }
-    public void setInicioArista(Arista inicioArista){
+
+    public void setInicioArista(Arista inicioArista) {
         this.inicioArista = inicioArista;
     }
+<<<<<<< HEAD
 }
+
+=======
+}
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
