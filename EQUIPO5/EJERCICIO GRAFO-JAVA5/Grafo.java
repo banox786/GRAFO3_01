3e7b8b34a3c7eddb3950f8cd334b1858027addc6
IRojit0 <159846136+AleMaPa0@users.noreply.github.com>
2024-07-21 13:47:19 -0600

    Update Grafo.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Grafo.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Grafo.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Grafo{
 
     private Vertice inicio;
@@ -13,12 +15,35 @@ public class Grafo{
     public void setInicio(Vertice inicio){
         this.inicio = inicio;
     }
-    
+
+    private void insertarVertice(int capacidad, String tipo, String ubicacion){
+       Vertice nuevo = new Vertice(capacidad, tipo, ubicacion);
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int opc;
+        int capacidad;
+        Vertice origen, destino;
+        Scanner leer = new Scanner(System.in);
+        Grafo grafo = new Grafo();
+
+        do{
+            System.out.print("1. Insertar vertice");
+            System.out.print("2. Insertar arista");
+            System.out.print("3. Mostrar");
+            System.out.print("4. Salir");
+            opc=leer.nextInt();
+            switch(opc){
+                case 1 -> {
+                    System.out.print("Ingresa los datos del vertice");
+                    capacidad = leer.next().intAt(0);
+                }
+            }
+        }while(opc!=4);
+        
     }
 }
