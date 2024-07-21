0f49f81db3bf9c0c4da5f0579591f2664c992803
Keila Aranda02 <keilaaranda@micorreo.upp.edu.mx>
2024-07-21 13:56:14 -0600

    Update Vertice.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Vertice.java
@@ -1,32 +1,56 @@
 public class Vertice {
-    private int dato;
+    private String capacidad;
+    private String tipo;
+    private String ubicacion;
     private Vertice sigVertice;
     private Arista inicioArista;
 
-    public Vertice(int dato){
-        this.dato = dato;
+    public Vertice(String capacidad, String tipo, String ubicacion) {
+        this.capacidad = capacidad;
+        this.tipo = tipo;
+        this.ubicacion = ubicacion;
         this.sigVertice = null;
         this.inicioArista = null;
     }
 
-    public int getDato(){
-        return dato;
-    }
-    public void setDato(int dato){
-        this.dato = dato;
+    public String getCapacidad() {
+        return capacidad;
     }
 
-    public Vertice getSigVertice(){
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Vertice getSigVertice() {
         return sigVertice;
     }
-    public void setSigVertice(Vertice sigVertice){
+
+    public void setSigVertice(Vertice sigVertice) {
         this.sigVertice = sigVertice;
     }
 
-    public Arista getInicioArista(){
+    public Arista getInicioArista() {
         return inicioArista;
     }
-    public void setInicioArista(Arista inicioArista){
+
+    public void setInicioArista(Arista inicioArista) {
         this.inicioArista = inicioArista;
     }
 }
+
