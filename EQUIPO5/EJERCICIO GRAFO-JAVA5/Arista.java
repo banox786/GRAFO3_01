5081e576cc59117b62529dba944d5997c8ab863a
IRojit0 <159846136+AleMaPa0@users.noreply.github.com>
2024-07-21 14:08:30 -0600

    Update Arista.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Arista.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Arista.java
@@ -3,7 +3,13 @@ public class Arista {
     private int distancia;
     private Vertice verticeDestino;
     private Arista sigArista;
+    private int flujo;
+    private int distancia;
+    private String presion;
 
+<<<<<<< HEAD
+    public Arista(Vertice verticeDestino, int flujo, int distancia, String presion) {
+=======
     public int getFlujo() {
         return flujo;
     }
@@ -30,22 +36,55 @@ public class Arista {
 
 
     public Arista(Vertice verticeDestino){
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
         this.verticeDestino = verticeDestino;
         this.sigArista = null;
+        this.flujo = flujo;
+        this.distancia = distancia;
+        this.presion = presion;
     }
 
-    public Vertice getVerticeDestino(){
+    public Vertice getVerticeDestino() {
         return verticeDestino;
     }
-    public void setVerticeDestino(Vertice verticeDestino){
+
+    public void setVerticeDestino(Vertice verticeDestino) {
         this.verticeDestino = verticeDestino;
     }
 
-    public Arista getSigArista(){
+    public Arista getSigArista() {
         return sigArista;
     }
 
-    public void setSigArista(Arista sigArista){
+    public void setSigArista(Arista sigArista) {
         this.sigArista = sigArista;
     }
+<<<<<<< HEAD
+
+    public int getFlujo() {
+        return flujo;
+    }
+
+    public void setFlujo(int flujo) {
+        this.flujo = flujo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public String getPresion() {
+        return presion;
+    }
+
+    public void setPresion(String presion) {
+        this.presion = presion;
+    }
 }
+=======
+}
+>>>>>>> ab325d34e87dc47453b2f057a2c04f80975d49fa
