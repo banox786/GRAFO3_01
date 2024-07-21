35d7e7821814e3a61e90f6f364b97c7b387593ad
IRojit0 <159846136+AleMaPa0@users.noreply.github.com>
2024-07-21 13:47:17 -0600

    Update Arista.java

--- EQUIPO5/EJERCICIO GRAFO-JAVA5/Arista.java
+++ EQUIPO5/EJERCICIO GRAFO-JAVA5/Arista.java
@@ -1,6 +1,33 @@
 public class Arista {
+    private int flujo;
+    private int distancia;
     private Vertice verticeDestino;
     private Arista sigArista;
+
+    public int getFlujo() {
+        return flujo;
+    }
+
+    public void setFlujo(int flujo) {
+        this.flujo = flujo;
+    }
+
+    public int getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(int distancia) {
+        this.distancia = distancia;
+    }
+
+    public int getPresion() {
+        return presion;
+    }
+
+    public void setPresion(int presion) {
+        this.presion = presion;
+    }
+
 
     public Arista(Vertice verticeDestino){
         this.verticeDestino = verticeDestino;
