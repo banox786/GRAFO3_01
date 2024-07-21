5ab41a829411f518ef1d547b4367a7287fefc846
Keila Aranda02 <keilaaranda@micorreo.upp.edu.mx>
2024-07-21 14:05:21 -0600

    Create Vertice.java

---
+++ EQUIPO5/Grafo1/src/grafo1/Vertice.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package grafo1;
+
+/**
+ *
+ * @author Keila
+ */
+
+public class Vertice {
+    private String capacidad;
+    private String tipo;
+    private String ubicacion;
+    private Vertice sigVertice;
+    private Arista inicioArista;
+
+    public Vertice(String capacidad, String tipo, String ubicacion) {
+        this.capacidad = capacidad;
+        this.tipo = tipo;
+        this.ubicacion = ubicacion;
+        this.sigVertice = null;
+        this.inicioArista = null;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public Vertice getSigVertice() {
+        return sigVertice;
+    }
+
+    public void setSigVertice(Vertice sigVertice) {
+        this.sigVertice = sigVertice;
+    }
+
+    public Arista getInicioArista() {
+        return inicioArista;
+    }
+
+    public void setInicioArista(Arista inicioArista) {
+        this.inicioArista = inicioArista;
+    }
+}
