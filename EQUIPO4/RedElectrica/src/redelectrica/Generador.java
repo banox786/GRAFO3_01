8595b9d4c65fd353f7b55f83a7fabe377d40e53a
penaabraham <penaabraham@micorreo.upp.edu.mx>
2024-07-23 12:51:27 -0600

    Create Generador.java

---
+++ EQUIPO4/RedElectrica/src/redelectrica/Generador.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package redelectrica;
+
+/**
+ *
+ * @author nuti_
+ */
+public class Generador {
+    
+}
