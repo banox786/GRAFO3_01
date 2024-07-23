d8baf3abf7aa4a590c35540af28dd2ba986aebb4
KDiaz1535 <162596587+KDiaz1535@users.noreply.github.com>
2024-07-23 13:12:25 -0600

    Update Grafo.java

--- EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
+++ EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
@@ -2,7 +2,43 @@ package bankingmovement;
 
 import java.util.List;
 
-
 public class Grafo {
-   
+   private List<Vertice> vertices;
+   private List<Arista> aristas;
+
+public grafo(List<Vertice> vertices, List<Arista> Aristas){
+   this.vertices = vertices;
+   this. aristas = aristas;
+   }
+   public List<Vertice> getVertices(){
+      return vertices;
+   }
+   public void setVertices(List<Vertice> vertices){
+      this.vertices = vertices;
+   }
+   public List<Arista> getArista(){
+      return aristas;
+   }
+   public void setAristas(List<Arista> aristas){
+      this.aristas = aristas;
+   }
+
+   public void insertarArista(Vertice origen, Vertice destino){
+      if (origen == null || destino == null){
+         System.out.println("No podemos insertar arista, intentalo de nuevo");
+      }else{
+      Arista nuevaArista = new Arista(destino);
+         if (origen.getInicioArista() == null){
+            origen.setInicioArista(nuevaArista);
+         }else{
+            Arista actual = origen.getInicioArista();
+            while (actual.getSigArista() != null);
+               actual = actual.getSigArista();
+         }
+         actual.setSigArista(nuevaArista);
+      }
+   }
 }
+
+   public void mostrarListaAdyacencia
+         
