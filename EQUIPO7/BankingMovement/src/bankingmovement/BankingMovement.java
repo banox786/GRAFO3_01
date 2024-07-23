77bd3bd9a38e903b50ab5bff9c282c226bbdc82c
Angel Espinosa <145628317+VoidEren@users.noreply.github.com>
2024-07-23 13:30:51 -0600

    Update BankingMovement.java
    Se implemento el menú de opciones para la clase principal.

--- EQUIPO7/BankingMovement/src/bankingmovement/BankingMovement.java
+++ EQUIPO7/BankingMovement/src/bankingmovement/BankingMovement.java
@@ -1,1 +1,32 @@
+package bakingmovement;
+import java.util.Scanner;
 
+public class BankingMovement {
+
+  public static void main(String[] args) {
+    int opc;
+    Vertice origen, destino;
+    Scanner read = new Scanner(System.in);
+
+  do{
+    System.out.println("1. Insertar Vertice");
+    System.out.println("2. Insertar Arista");
+    System.out.println("3. Mostrar");
+    System.out.println("4. Salir");
+    opc = read.nextInt();
+    switch(opc){
+      case 1:
+        breaK;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        System.out.println("Saliendo del programa...");
+        break;
+      default:
+        System.out.println("Opcion no vaida");
+    }
+  }while (opc !=4);
+  }
+}
