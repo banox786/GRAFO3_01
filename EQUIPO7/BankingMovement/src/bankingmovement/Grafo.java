2ba41a49ddb7866447e1e1ff58f55c9828e617db
R3bec <162595504+R3bec@users.noreply.github.com>
2024-07-23 12:25:05 -0600

    Update Grafo.java
    Se creo la clase grafo

--- EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
+++ EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
@@ -4,30 +4,5 @@ import java.util.List;
 
 
 public class Grafo {
-    private List <Vertice> vertices;
-    private List <Arista> aristas;
-
-    public Grafo(List<Vertice> vertices, List<Arista> aristas) {
-        this.vertices = vertices;
-        this.aristas = aristas;
-    }
-
-    public List<Vertice> getVertices() {
-        return vertices;
-    }
-
-    public void setVertices(List<Vertice> vertices) {
-        this.vertices = vertices;
-    }
-
-    public List<Arista> getAristas() {
-        return aristas;
-    }
-
-    public void setAristas(List<Arista> aristas) {
-        this.aristas = aristas;
-    }
-    
-    
-
+   
 }
