95b59c51222bfba5a851cc018bd453fe7cd32378
Ilderios <ilderios030405@micorreo.upp.edu.mx>
2024-07-22 22:22:24 -0600

    Update Grafo.java

--- EQUIPO5/Grafo/src/grafo/Grafo.java
+++ EQUIPO5/Grafo/src/grafo/Grafo.java
@@ -23,58 +23,88 @@ public class Grafo{
     public void setInicio(Vertice inicio){
         this.inicio = inicio;
     }
-    //Método de insertarVertice
+    //Método de 'insertarVertice' con los parametros 'capacodad', 'tipo', y 'ubicacion'.
     public void insertarVertice(char capacidad, char tipo, char ubicacion) {
         Vertice nuevo = new Vertice(capacidad, tipo, ubicacion);
+        //Si la la lista del vértice está vacia, el nuevo vértice se convierte en el primer vértice de la lista.
         if (this.inicio == null) {
             this.inicio = nuevo;
+        //Si la lista no está vacía, el código busca el ultimo vértice de la lista.
+        //Se inicializa la variable actual con el vertice ('this.inicio')
         } else {
             Vertice actual = this.inicio;
+            /*Se recorre la lista de vértices usando un bucle while, avanzando al siguiente vértice hasta encontrar el ultimo vértice,
+            osea que el ultimo vértice sea igual a 'NULL'.*/
             while (actual.getSigVertice() != null) {
                 actual = actual.getSigVertice();
             }
+            //Una ves encontrado el último vértice se establece 'nuevo' ahora como 'SigVertice'.
             actual.setSigVertice(nuevo);
         }
     }
+    //Método de 'insertarArista' con los parametros 'origen', 'destino', 'flujo', 'distancia', 'presion'.
     public void insertarArista(char origen, char destino, int flujo, int distancia, char presion) {
+        //Usa el metodo 'buscarVertice' para buscar los vértices de origen.
         Vertice verticeOrigen = buscarVertice(origen);
         Vertice verticeDestino = buscarVertice(destino);
+        //Se verifica que ambos vértices existan en el grafo.
         if (verticeOrigen != null && verticeDestino != null) {
+            //Se crea un instancia de 'Arista' con los parametros proporcionados antes proporcionados, apuntando a vértice destino con las propiedades
             Arista nuevaArista = new Arista(verticeDestino, flujo, distancia, presion);
+            //Si el vértice de origen no tiene aristas se estableve la nueva arista como la primera.
             if (verticeOrigen.getInicioArista() == null) {
                 verticeOrigen.setInicioArista(nuevaArista);
             } else {
+                //Si ya hay aristas, se recorre la lista de origen hata encontrar la ultima y se añade la nueva arista al final
                 Arista actual = verticeOrigen.getInicioArista();
                 while (actual.getSigArista() != null) {
                     actual = actual.getSigArista();
                 }
                 actual.setSigArista(nuevaArista);
             }
+            // Si no existen ninguno de los vértices mandara ese mensaje.
         } else {
             System.out.println("Uno o ambos vértices no existen.");
         }
     }
+    //Método mostras con los parametros de capacidad
     private Vertice buscarVertice(char capacidad) {
+        //Se inicializa una variable actual tipo 'Vertice' con el primer vértice de la lista.
         Vertice actual = this.inicio;
+        // Se recorre la lista de vértices mientras 'actual' no sea igual a 'null'.
         while (actual != null) {
+            // Se verifica si la capacidad del vértice actual es igual a la capacidad buscada.
             if (actual.getCapacidad() == capacidad) {
+                // Si se encuentra el vértice con la capacidad buscada, se retorna ese vértice.
                 return actual;
             }
+            //Se avanza con el sigueinte vértice de la lista.
             actual = actual.getSigVertice();
         }
+        //Si no encuentra ningun vértice con la capacidad buscada, se retorna NULL.
         return null;
     }
     //Método de mostrar
 public void mostrarListaAdyacencia() {
+    // Inicializa 'actual' con el primer vértice de la lista
         Vertice actual = this.getInicio();
+         // Recorre la lista de vértices mientras 'actual' no sea igual a 'null'
         while (actual != null) {
+             // Imprime la capacidad del vértice actual seguida de ': '
             System.out.print(actual.getCapacidad() + ": ");
+             // Inicializa 'arista' con la primera arista del vértice actual
             Arista arista = actual.getInicioArista();
+            
+            // Recorre la lista de aristas del vértice actual
             while (arista != null) {
+                 // Imprime la capacidad del vértice destino de la arista actual seguido de un espacio
                 System.out.print(arista.getVerticeDestino().getCapacidad() + " ");
+                 // Avanza a la siguiente arista
                 arista = arista.getSigArista();
             }
+            // Imprime una nueva línea al final de la lista de aristas del vértice actual
             System.out.println();
+            // Avanza al siguiente vértice en la lista
             actual = actual.getSigVertice();
         }
     }
@@ -89,6 +119,7 @@ public void mostrarListaAdyacencia() {
         Grafo grafo = new Grafo();
         
         do {
+            // Menú que se mostrara al usuario
             System.out.println("1. Insertar vertice");
             System.out.println("2. Insertar arista");
             System.out.println("3. Mostrar");
