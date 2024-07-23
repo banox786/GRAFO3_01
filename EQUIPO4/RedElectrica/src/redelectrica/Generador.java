df8f888eb9432e79d7241a80ecf65972e70da338
KDiaz1535 <162596587+KDiaz1535@users.noreply.github.com>
2024-07-23 13:12:15 -0600

    Update Generador.java

--- EQUIPO4/RedElectrica/src/redelectrica/Generador.java
+++ EQUIPO4/RedElectrica/src/redelectrica/Generador.java
@@ -9,5 +9,42 @@ package redelectrica;
  * @author nuti_
  */
 public class Generador {
+    private int id;
+    private int capacidad;
+    private String tipo;
+    private String ubicacion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+    
     
 }
