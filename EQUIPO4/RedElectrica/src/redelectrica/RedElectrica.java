0af11f97a907aa9f3260f70532d27709d8d74a2d
Xime <162523206+dixtom@users.noreply.github.com>
2024-07-23 17:34:09 -0600

    Update RedElectrica.java

--- EQUIPO4/RedElectrica/src/redelectrica/RedElectrica.java
+++ EQUIPO4/RedElectrica/src/redelectrica/RedElectrica.java
@@ -9,10 +9,49 @@ package redelectrica;
  * @author Diana Ximena
  */
 public class RedElectrica {
-private Generador inicio;
-    /**
-     * @param args the command line arguments
-     */
+ private Generador inicio;
+ 
+ 
+ 
+ 
+ 
+
+    public Generador getInicio() {
+        return inicio;
+    }
+  
+    public void setInicio(Generador inicio) {
+        this.inicio = inicio;
+    }
+    
+ 
+ 
+ 
+    
+    
+    //Incertar Generador
+    public void insertarGenerador(int id, int capacidad, String tipo, String ubicacion){
+        //Crear nuevo Generador
+        Generador nuevo = new Generador(id, capacidad, tipo, ubicacion);
+        
+        //Si el grafo esta vacio
+        if(this.getInicio() == null){
+            this.setInicio(nuevo);
+            
+        //Si ya existe al menos un elemento
+        }else{
+            Generador actual = this.getInicio();
+            
+            //Recorrido al final
+            while(actual.getSigGenerador() != null){
+                actual = actual.getSigGenerador();
+            }
+            actual.setSigGenerador(nuevo);
+        }  
+    }
+ 
+ 
+ 
     public static void main(String[] args) {
         int a;
     }
