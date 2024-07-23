34fc13301b50e071a7dde23f3b08bd7c454bdb9e
Kimm <162529310+KIMM24@users.noreply.github.com>
2024-07-23 17:52:50 -0600

    Update RedElectrica.java

--- EQUIPO4/RedElectrica/src/redelectrica/RedElectrica.java
+++ EQUIPO4/RedElectrica/src/redelectrica/RedElectrica.java
@@ -49,6 +49,39 @@ public class RedElectrica {
             actual.setSigGenerador(nuevo);
         }  
     }
+    
+    //Mostrar lista de Adyacencia
+    public void mostrarListaAdyacencia(){
+        //Declara Generador actual
+        Generador actual = this.getInicio();
+        while(actual != null){
+            //Imprime el ID del generador actual
+            //System.out.print(actual.getId() + ": ");
+            System.out.print("ID=" + actual.getId() + 
+                             "{Capacidad=" + actual.getCapacidad() + 
+                             ", Tipo=" + actual.getTipo() + 
+                             ", Ubicacion=" + actual.getUbicacion() + 
+                             "}: ");
+            //Inicia con el primer cable del generador actual
+            Cable actualCab = actual.getInicioCable();
+            //Recorre todos los cables del generador actual
+            while(actualCab != null){
+                //Imprime el dato del generador destino del cable
+                //System.out.print(actualCab.getGeneradorDestino().getId() + " -> ");
+                System.out.print("ID=" + actualCab.getGeneradorDestino().getId() + 
+                             "{Capacidad=" + actualCab.getGeneradorDestino().getCapacidad() + 
+                             ", Tipo=" + actualCab.getGeneradorDestino().getTipo() + 
+                             ", Ubicacion=" + actualCab.getGeneradorDestino().getUbicacion() + 
+                             "} -> ");
+                //Pasa a el siguiente cable
+                actualCab = actualCab.getSigCable();
+            }
+            //Salto de línea para el siguiente generador
+            System.out.println("");
+            //Pasa al siguiente generador
+            actual = actual.getSigGenerador();
+        }
+    }
  
  
  
