8b6da4ae1f3208bcdedec4368714ae5baafad894
Nahum Mejia <162595000+NahumMejia@users.noreply.github.com>
2024-07-23 21:08:21 -0600

    Update Grafo.java
    Hice el método mostrar lista de adyacencia, puede estar sujeta a cambios después de hacer el main, dependiendo de la visualización de este metodo en la consola.

--- EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
+++ EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
@@ -27,16 +27,21 @@ public class Grafo {
             actual.setSigVertice(nuevo);
         }
     }
-    public void insertarVertice(String nombre, String ciudad, int seguidores){
-            Vertice nuevo = new Vertice(nombre, ciudad, seguidores);
-            if(this.getInicio()==null){
-                this.setInicio(nuevo);
-            }else{
-                Vertice actual = this.getInicio();
-                while(actual.getSigVertice()!=null){
-                    actual = actual.getSigVertice();
-                }
-                actual.setSigVertice(nuevo);
-            }
+    public void mostrarListaAdyacencia() {
+    Vertice actual = this.getInicio();
+    while (actual != null) {
+        System.out.print("Ciudad: " + actual.getCiudad() + ", ");
+        System.out.print("Nombre: " + actual.getNombre() + ", ");
+        System.out.println("Seguidores: " + actual.getSeguidores());
+        Arista actualArista = actual.getInicioArista();
+        while (actualArista != null) {
+            System.out.print("  -> Ciudad: " + actualArista.getVerticeDestino().getCiudad() + ", ");
+            System.out.print("Nombre: " + actualArista.getVerticeDestino().getNombre() + ", ");
+            System.out.println("Seguidores: " + actualArista.getVerticeDestino().getSeguidores());
+            actualArista = actualArista.getSigArista();
         }
+        actual = actual.getSigVertice();
+        System.out.println("");
+        }
+    }
 }
