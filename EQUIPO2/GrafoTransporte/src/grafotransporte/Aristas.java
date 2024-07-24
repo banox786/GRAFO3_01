a720fbd3662165443a6797395d42a34cab3a7296
Emmanuelmg19 <78052526+Emmanuelmg19@users.noreply.github.com>
2024-07-23 19:55:33 -0600

    CLASE ARISTAS

--- EQUIPO2/GrafoTransporte/src/grafotransporte/Aristas.java
+++ EQUIPO2/GrafoTransporte/src/grafotransporte/Aristas.java
@@ -9,5 +9,38 @@ package grafotransporte;
  * @author navy_
  */
 public class Aristas {
+    Vertice destino;
+    double distancia;
+    double tiempo;
+
+    public Aristas(Vertice destino, double distancia, double tiempo) {
+        this.destino = destino;
+        this.distancia = distancia;
+        this.tiempo = tiempo;
+    }
+
+    public Vertice getDestino() {
+        return destino;
+    }
+
+    public void setDestino(Vertice destino) {
+        this.destino = destino;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
     
 }
