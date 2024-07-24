e990ce6c96b1ed70f476ceabf4c74b7862372a97
Antonio98g <167117462+Antonio98g@users.noreply.github.com>
2024-07-23 20:17:53 -0600

    Update Grafo.java
    se agregaron los getters y setters asi como tambien la funcion para insertar el vertice

--- EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
+++ EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
@@ -10,5 +10,29 @@ package graforedsocial;
  * @author HOME
  */
 public class Grafo {
+    private Vertice inicio;
+
+    public Grafo(){
+        this.inicio = null;
+    }
+
+    public Vertice getInicio(){
+        return inicio;
+    }
     
+    public void setInicio(){
+        this.inicio = inicio;
+    }
+
+    public void insertarVertice(String nombre, String ciudad, int seguidores){
+        Vertice nuevo = new Vertice(nombre, ciudad, seguidores);
+        if(this.getInicio()==null){
+            this.setInicio(nuevo);
+        }else{
+            Vertice actual = this.getInicio();
+            while(actual.getSigVertice()!=null){
+                actual = actual.getSigVertice();
+            }
+            actual.setSigVertice(nuevo);
+    }
 }
