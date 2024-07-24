e885c0355e8c704ba44dd8abe6e471820e3124a7
JulioH5 <162396220+Ixtervic@users.noreply.github.com>
2024-07-24 08:43:01 -0600

    Clase Dispositivo

---
+++ EQUIPO1/RedTelecomunicaciones/src/main/java/com/mycompany/redtelecomunicaciones/Dispositivo.java
@@ -0,0 +1,77 @@
+package com.mycompany.redtelecomunicaciones;
+
+/**
+ *
+ * @author julio
+ */
+public class Dispositivo {
+    private String dirIP;
+    private String nombre;
+    private String tipo;
+    private float capacidad;
+    private Dispositivo sigDispositivo;
+    Arista primerArista;
+
+    public Dispositivo(String dirIP, String nombre, String tipo, float capacidad) {
+        this.dirIP = dirIP;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.capacidad = capacidad;
+        this.sigDispositivo = null;
+        this.primerArista = null;
+    }
+
+    public String getDirIP() {
+        return dirIP;
+    }
+
+    public void setDirIP(String dirIP) {
+        this.dirIP = dirIP;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public float getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(float capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public Dispositivo getSigDispositivo() {
+        return sigDispositivo;
+    }
+
+    public void setSigDispositivo(Dispositivo sigDispositivo) {
+        this.sigDispositivo = sigDispositivo;
+    }
+
+    public Arista getPrimerArista() {
+        return primerArista;
+    }
+
+    public void setPrimerArista(Arista primerArista) {
+        this.primerArista = primerArista;
+    }
+
+    @Override
+    public String toString() {
+        return "IP: " + dirIP + "\nNombre dispositivo: " + nombre + "\nTipo: " + tipo + "\nCapacidad: " + capacidad;
+    }
+
+}
