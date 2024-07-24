d79674ad45eb0c927f7d7bea86945136e60e5109
Rom <160189542+STARKILL117rm@users.noreply.github.com>
2024-07-24 09:28:12 -0600

    Update Vertice.java
    Actualizacion de la clase Vertice

--- EQUIPO7/BankingMovement/src/bankingmovement/Vertice.java
+++ EQUIPO7/BankingMovement/src/bankingmovement/Vertice.java
@@ -1,5 +1,17 @@
 package bankingmovement;
 
 public class Vertice {
-   
+
+    private String id;
+    private String numeroCuenta;
+    private String tipoCuenta;
+    private float saldo;
+    private Vertice sigVertice;
+    private Arista inicioArista;
+
+   public Vertice(String id, String numeroCuenta, String tipoCuenta, float Saldo) {
+      this.id = id;
+      this.numeroCuenta = numeroCuenta;
+this.tipoCuenta = tipoCuenta
+
 }
