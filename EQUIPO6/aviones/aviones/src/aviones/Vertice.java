1026beee200ec3608b79285d03e90465195b9de9
Paulinomed <paulinomedina484@gmail.com>
2024-07-24 12:25:42 -0600

    cambios en el constructot

--- EQUIPO6/aviones/aviones/src/aviones/Vertice.java
+++ EQUIPO6/aviones/aviones/src/aviones/Vertice.java
@@ -5,33 +5,6 @@ public class Vertice {
     private Vertice siguienteVertice;
     private Arista inicio;
 
-    public Vertice(char dato) {
-        this.dato = dato;
-        this.siguienteVertice = null;
-        this.inicio = null;
-    }
-
-    public char getDato() {
-        return dato;
-    }
-
-    public void setDato(char dato) {
-        this.dato = dato;
-    }
-
-    public Vertice getSiguienteVertice() {
-        return siguienteVertice;
-    }
-
-    public void setSiguienteVertice(Vertice siguienteVertice) {
-        this.siguienteVertice = siguienteVertice;
-    }
-
-    public Arista getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(Arista inicio) {
-        this.inicio = inicio;
-    }
+    
+    
 }
