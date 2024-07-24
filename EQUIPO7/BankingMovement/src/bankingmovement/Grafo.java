1f68617696b8ed004b6bb76f171f881a74716070
Melany Fuentes <jacquelinefuentes@micorreo.upp.edu.mx>
2024-07-24 09:23:54 -0600

    Se realizó la clase Grafo
    se hicieron funciones y su constructor, getter y setter

--- EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
+++ EQUIPO7/BankingMovement/src/bankingmovement/Grafo.java
@@ -1,44 +1,73 @@
 package bankingmovement;
 
-import java.util.List;
-
 public class Grafo {
-   private List<Vertice> vertices;
-   private List<Arista> aristas;
+    private Vertice inicio;
 
-public grafo(List<Vertice> vertices, List<Arista> Aristas){
-   this.vertices = vertices;
-   this. aristas = aristas;
-   }
-   public List<Vertice> getVertices(){
-      return vertices;
-   }
-   public void setVertices(List<Vertice> vertices){
-      this.vertices = vertices;
-   }
-   public List<Arista> getArista(){
-      return aristas;
-   }
-   public void setAristas(List<Arista> aristas){
-      this.aristas = aristas;
-   }
+    public Grafo() {
+        this.inicio = null;
+    }
 
-   public void insertarArista(Vertice origen, Vertice destino){
-      if (origen == null || destino == null){
-         System.out.println("No podemos insertar arista, intentalo de nuevo");
-      }else{
-      Arista nuevaArista = new Arista(destino);
-         if (origen.getInicioArista() == null){
-            origen.setInicioArista(nuevaArista);
-         }else{
-            Arista actual = origen.getInicioArista();
-            while (actual.getSigArista() != null);
-               actual = actual.getSigArista();
-         }
-         actual.setSigArista(nuevaArista);
-      }
-   }
+    public Vertice getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Vertice inicio) {
+        this.inicio = inicio;
+    }
+
+    public void insertarArista(Arista arista) {
+        if (arista.getCuentaOrigen() == null || arista.getCuentaDestino() == null) {
+            System.out.println("No se puede insertar la arista");
+        } else {
+            Vertice cuentaOrigen = arista.getCuentaOrigen();
+            if (cuentaOrigen.getInicioArista() == null) {
+                cuentaOrigen.setInicioArista(arista);
+            } else {
+                Arista actual = cuentaOrigen.getInicioArista();
+                while (actual.getSigArista() != null) {
+                    actual = actual.getSigArista();
+                }
+                actual.setSigArista(arista);
+            }
+        }
+    }
+
+    public void insertarVertice(Vertice vertice) {
+        if (this.inicio == null) {
+            this.inicio = vertice;
+        } else {
+            Vertice actual = this.inicio;
+            while (actual.getSigVertice() != null) {
+                actual = actual.getSigVertice();
+            }
+            actual.setSigVertice(vertice);
+        }
+    }
+
+    public Vertice buscarVertice(String id) {
+        Vertice actual = this.inicio;
+        while (actual != null) {
+            if (actual.getId().equals(id)) {
+                return actual;
+            } else {
+                actual = actual.getSigVertice();
+            }
+        }
+        return null;
+    }
+
+    public void mostrarListaAdyacencia() {
+        Vertice actual = this.inicio;
+        while (actual != null) {
+            System.out.print(actual.getId() + ": ");
+            Arista actualArista = actual.getInicioArista();
+            while (actualArista != null) {
+                System.out.print(actualArista.getCuentaDestino().getId() + " -> ");
+                actualArista = actualArista.getSigArista();
+            }
+            System.out.println("");
+            actual = actual.getSigVertice();
+        }
+    }
 }
-
-   public void mostrarListaAdyacencia
          
