49bf10913869237631920697d83ff0d591b7c832
Antonio98g <167117462+Antonio98g@users.noreply.github.com>
2024-07-24 09:02:58 -0600

    Update Grafo.java
    Hice algunos cambios en la version anterior que incluyen algunas mejoras y esta seria la version final del codigo.

--- EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
+++ EQUIPO3/GrafoRedSocial/src/graforedsocial/Grafo.java
@@ -3,58 +3,74 @@ package graforedsocial;
 public class Grafo {
     private Vertice inicio;
 
-    public Grafo(){
+    public Grafo() {
         this.inicio = null;
     }
 
-    public Vertice getInicio(){
+    public Vertice getInicio() {
         return inicio;
     }
-        public void setInicio(){
+
+    public void setInicio(Vertice inicio) {
         this.inicio = inicio;
     }
 
-    public void insertarVertice(String nombre, String ciudad, int seguidores){
+    public void insertarVertice(String nombre, String ciudad, int seguidores) {
         Vertice nuevo = new Vertice(nombre, ciudad, seguidores);
-        if(this.getInicio()==null){
+        if (this.getInicio() == null) {
             this.setInicio(nuevo);
-        }else{
+        } else {
             Vertice actual = this.getInicio();
-            while(actual.getSigVertice()!=null){
+            while (actual.getSigVertice() != null) {
                 actual = actual.getSigVertice();
             }
             actual.setSigVertice(nuevo);
+        }
     }
+
     public void mostrarListaAdyacencia() {
         Vertice actual = this.getInicio();
-            while (actual != null) {
-                System.out.print("Ciudad: " + actual.getCiudad() + ", ");
-                System.out.print("Nombre: " + actual.getNombre() + ", ");
-                System.out.println("Seguidores: " + actual.getSeguidores());
-                Arista actualArista = actual.getInicioArista();
-                while (actualArista != null) {
-                    System.out.print("  -> Ciudad: " + actualArista.getVerticeDestino().getCiudad() + ", ");
-                    System.out.print("Nombre: " + actualArista.getVerticeDestino().getNombre() + ", ");
-                    System.out.println("Seguidores: " + actualArista.getVerticeDestino().getSeguidores());
-                    actualArista = actualArista.getSigArista();
-                    }
-                actual = actual.getSigVertice();
-                System.out.println("");
+        while (actual != null) {
+            System.out.print("Ciudad: " + actual.getCiudad() + ", ");
+            System.out.print("Nombre: " + actual.getNombre() + ", ");
+            System.out.println("Seguidores: " + actual.getSeguidores());
+            Arista actualArista = actual.getInicioArista();
+            while (actualArista != null) {
+                System.out.print("  -> Ciudad: " + actualArista.getVerticeDestino().getCiudad() + ", ");
+                System.out.print("Nombre: " + actualArista.getVerticeDestino().getNombre() + ", ");
+                System.out.println("Seguidores: " + actualArista.getVerticeDestino().getSeguidores());
+                actualArista = actualArista.getSigArista();
+            }
+            actual = actual.getSigVertice();
+            System.out.println("");
+        }
+    }
+
+    public void insertarArista(Vertice origen, Vertice destino) {
+        if (origen == null || destino == null) {
+            System.out.println("No se puede insertar la arista");
+        } else {
+            Arista nuevaArista = new Arista(destino);
+            if (origen.getInicioArista() == null) {
+                origen.setInicioArista(nuevaArista);
+            } else {
+                Arista actual = origen.getInicioArista();
+                while (actual.getSigArista() != null) {
+                    actual = actual.getSigArista();
                 }
+                actual.setSigArista(nuevaArista);
             }
-    public void insertarArista(Vertice origen, Vertice destino){
-    if(origen==null || destino==null){
-        System.out.println("No se puede conectar a arista");
-    }else{
-          Arista nuevaArista=new Arista(destino);
-          if(origen.getInicioArista()==null){
-          origen.setInicioArista(nuevaArista);
-          }else{
-            Arista actual=origen.getInicioArista();
-            while(actual.getSigArista()!=null){
-                actual=actual.getSigArista();
+        }
+    }
+
+    public Vertice buscarVertice(String nombre) {
+        Vertice actual = this.getInicio();
+        while (actual != null) {
+            if (actual.getNombre().equals(nombre)) {
+                return actual;
             }
-            actual.setSigArista(nuevaArista);
-          }
+            actual = actual.getSigVertice();
+        }
+        return null; 
     }
 }
