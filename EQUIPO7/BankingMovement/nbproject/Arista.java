80224f5cc4fee265b92f54b7a5e3218d275e307f
Rom <160189542+STARKILL117rm@users.noreply.github.com>
2024-07-24 09:11:30 -0600

    Add files via upload
    resubida de la clase arista

---
+++ EQUIPO7/BankingMovement/nbproject/Arista.java
@@ -0,0 +1,55 @@
+public class Arista{
+    private float monto;
+    private String fecha;
+    private String tipoTransaccion;
+    private Vertice cuentaOrigen;
+    private Vertice cuentaDestino;
+
+    public Arista(float monto, String fecha, String tipoTransaccion, Vertice cuentaOrigen, Vertice cuentaDestino){
+        this.monto = monto;
+        this.fecha = fecha;
+        this.tipoTransaccion = tipoTransaccion;
+        this.cuentaOrigen = null;
+        this.cuentaDestino = null;
+    }
+
+    public float getMonto(){
+        return monto;
+    }
+
+    public void setMonto(float monto){
+        this.monto = monto;
+    }
+
+    public String getFecha(){
+        return fecha;
+    }
+
+    public void setFecha(String fecha){
+        this.fecha = fecha;
+    }
+
+    public String getTipoTransaccion(){
+        return tipoTransaccion;
+    }
+
+    public void setTipoTransaccion(String tipoTransaccion){
+        this.tipoTransaccion = tipoTransaccion;
+    }
+
+    public Vertice getCuentaOrigen(){
+        return cuentaOrigen;
+    }
+
+    public void setCuentaOrigen(Vertice cuentaOrigen){
+        this.cuentaOrigen = cuentaOrigen;
+    }
+
+    public Vertice getCuentaDestino(){
+        return cuentaDestino;
+    }
+
+    public void setCuentaDestino(Vertice cuentaDestino){
+        this.cuentaDestino = cuentaDestino;
+    }
+}
