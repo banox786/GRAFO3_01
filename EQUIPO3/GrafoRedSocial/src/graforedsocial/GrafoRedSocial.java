b5207f3f88255a0ee112175eb373f73e4ada40e8
Crisls24 <162597276+Crisls24@users.noreply.github.com>
2024-07-23 22:45:24 -0600

    Update GrafoRedSocial.java
    complementación del main

--- EQUIPO3/GrafoRedSocial/src/graforedsocial/GrafoRedSocial.java
+++ EQUIPO3/GrafoRedSocial/src/graforedsocial/GrafoRedSocial.java
@@ -24,19 +24,19 @@ public class GrafoRedSocial {
         Vertice origen, destino;
         do {
             System.out.println("Menu\n");
-            System.out.println("1) Insertar Vertice");
-            System.out.println("2) Insertar Arista");
+            System.out.println("1) Insertar Usuario");
+            System.out.println("2) Insertar Parentezco");
             System.out.println("3) Mostrar");
             System.out.println("4) Salir");
             opc = leer.nextInt();
             switch (opc) {
                 case 1:
                      System.out.println("Insertar Vertice");
-                    System.out.println("Ingresa el nombre del nuevo vertice");
+                    System.out.println("Ingresa el nombre del nuevo usuario: ");
                     String nombre = leer.nextLine();
-                    System.out.println("Ingresa la ciudad del nuevo vertice");
+                    System.out.println("Ingresa la ciudad: ");
                     String ciudad = leer.nextLine();
-                    System.out.println("Ingresa la cantidad de seguidores del nuevo vertice");
+                    System.out.println("Ingresa la cantidad de seguidores del usuario: ");
                     int seguidores = leer.nextInt();
                     grafo.insertarVertice(nombre, ciudad, seguidores);
                     break;
