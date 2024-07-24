6e1c9266835951a8c2a67c63ef48ca6ea751f131
alann616 <156497761+alann616@users.noreply.github.com>
2024-07-23 22:16:49 -0600

    Update RedTelecom.java
    corrections to the 'RedTelecom'  class

--- EQUIPO1/RedTelecomunicaciones/src/main/java/com/mycompany/redtelecomunicaciones/RedTelecom.java
+++ EQUIPO1/RedTelecomunicaciones/src/main/java/com/mycompany/redtelecomunicaciones/RedTelecom.java
@@ -1,8 +1,73 @@
-SSpackage com.mycompany.redtelecomunicaciones;
+public class RedTelecomAlan {
+    private Dispositivo primerDispositivo;
 
-public class RedTelecom {
+    public RedTelecomAlan() {
+        this.primerDispositivo = null;
+    }
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public Dispositivo getPrimerDispositivo() {
+        return primerDispositivo;
+    }
+
+    public void setPrimerDispositivo(Dispositivo primerDispositivo) {
+        this.primerDispositivo = primerDispositivo;
+    }
+
+    public void agregarDispositivo(Dispositivo nuevoDispositivo) {
+        if (this.getPrimerDispositivo() == null) {
+            this.setPrimerDispositivo(nuevoDispositivo);
+        } else {
+            Dispositivo actual = primerDispositivo;
+            while (actual.getSigDispositivo() != null) {
+                actual = actual.getSigDispositivo();
+            }
+            actual.setSigDispositivo(nuevoDispositivo);
+        }
+    }
+
+    public boolean buscarDispositivo(String dirIP) {
+        Dispositivo actual = primerDispositivo;
+        while (actual != null) {
+            if (actual.getDirIP().equals(dirIP)) {
+                return actual;
+            } else {
+                actual = actual.getSigDispositivo();
+            }
+        }
+        System.out.println("Dispositivo no encontrado");
+        return false;
+    }
+
+    public void agregarArista(String dirIPDispositivoOrigen, String dirIPDispositivoDestino, float ponderacion) {
+        Dispositivo dispositivoOrigen = buscarDispositivo(dirIPDispositivoOrigen);
+        Dispositivo dispositivoDestino = buscarDispositivo(dirIPDispositivoDestino);
+        if (dispositivoOrigen == null || dispositivoDestino == null) {
+            System.out.println("Imposible agregar arista");
+        } else {
+            Arista nuevaArista = new Arista(dispositivoDestino, peso);
+            if (dispositivoOrigen.getPrimerArista() == null) {
+                dispositivoOrigen.setPrimerArista(nuevaArista);
+            } else {
+                Arista actual = dispositivoOrigen.getPrimerArista();
+                while (actual.getSigArista() != null) {
+                    actual = actual.getSigArista();
+                }
+                actual.setSigArista(nuevaArista);
+            }
+        }
+    }
+
+    public void mostrarListaAdyacencia() {
+        Dispositivo actual = primerDispositivo;
+        while (actual != null) {
+            System.out.println(actual.toString() + "\t");
+            Arista actualArista = actual.getPrimerArista();
+            while (actualArista != null) {
+                System.out.print(actualArista.getDispositivoDestino().getDirIP() + "--" + actualArista.getPonderacion() + "-->");
+                actualArista = actualArista.getSigArista();
+            }
+            System.out.println("");
+            actual = actual.getSigDispositivo();
+        }
     }
 }
