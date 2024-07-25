cdca27b10bc16fb7d051a3559665c3dabc580a16
15Tano <eltano193@gmail.com>
2024-07-25 13:05:41 -0600

    Revert "Delete EQUIPO6/aviones/aviones directory"
    This reverts commit e44c5f1ca8a2bc883510ef6ac4a2c0a74c655dc0.

---
+++ EQUIPO6/aviones/aviones/src/aviones/Grafo.java
@@ -0,0 +1,19 @@
+package aviones;
+
+public class Grafo {
+    private Vertice inicio;
+
+    public Grafo() {
+        this.inicio = null;
+    }
+
+    public Vertice getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Vertice inicio) {
+        this.inicio = inicio;
+    }
+
+  
+}
