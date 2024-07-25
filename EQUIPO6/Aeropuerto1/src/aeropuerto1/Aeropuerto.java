d1bc573f1e4fd393efe861588909f84f66b8d2eb
Alanblancas2305 <162598494+Alanblancas2305@users.noreply.github.com>
2024-07-25 13:21:01 -0600

    Update Aeropuerto.java
    Clase Aeropuerto(Vertice)

--- EQUIPO6/Aeropuerto1/src/aeropuerto1/Aeropuerto.java
+++ EQUIPO6/Aeropuerto1/src/aeropuerto1/Aeropuerto.java
@@ -9,5 +9,30 @@ package aeropuerto1;
  * @author kungl
  */
 public class Aeropuerto {
-   
+    private String nombre;
+    private String ciudad;
+    private int capacidad;
+
+    public Aeropuerto(String nombre, String ciudad, int capacidad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + ciudad + ")";
+    }
 }
