79cea5ae1b0c39baaf13e5000e44bcb4d5929453
owenchapa1010 <131843692+owenchapa1010@users.noreply.github.com>
2024-07-25 13:14:00 -0600

    Revert "Delete EQUIPO6 directory"
    This reverts commit 4bb5ab92d805f7ee952f193a0889cce1cb784529.

---
+++ EQUIPO6/Aeropuerto1/src/aeropuerto1/Aeropuerto.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aeropuerto1;
+
+/**
+ *
+ * @author kungl
+ */
+public class Aeropuerto {
+    private String nombre;
+    private String ciudad;
+    private int capacidad;
+
+    public Aeropuerto(String nombre, String ciudad, int capacidad) {
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+        this.capacidad = capacidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + ciudad + ")";
+    }
+}
