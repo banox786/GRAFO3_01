d42541d8d461af606ecdfc0dcc175701e26e4998
JimeneVictor <162595405+JimeneVictor@users.noreply.github.com>
2024-07-24 21:53:05 -0600

    menu

--- EQUIPO1/RedTelecomunicaciones/src/main/java/com/mycompany/redtelecomunicaciones/RedTelecomunicaciones.java
+++ EQUIPO1/RedTelecomunicaciones/src/main/java/com/mycompany/redtelecomunicaciones/RedTelecomunicaciones.java
@@ -1,80 +1,56 @@
-package com.mycompany.redtelecomunicaciones;
-
+import java.util.Scanner;
+/**
+ *victor
+ */
 public class RedTelecomunicaciones {
-
-        private Vertice inicio;
-
-    public Grafo() {
-        this.inicio = null;
-    }
-
-    public Vertice getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(Vertice inicio) {
-        this.inicio = inicio;
-    }
-        
-    
-    public void insertarVertice(char dato){
-        Vertice nuevo=new Vertice(dato);
-        if(this.getInicio()==null){
-            this.setInicio(nuevo);
-        }else{
-            Vertice actual = this.getInicio();
-            while(actual.getSigVertice()!= null){
-                actual=actual.getSigVertice();
-            }   
-            actual.setSigVertice(nuevo);
-        }
-    }
-    
-    public Vertice buscarVertice(char dato){
-            Vertice actual = this.getInicio();
-            while(actual!=null){
-                if(actual.getDato()==dato){
-                    return actual;
-                }else{
-                    actual = actual.getSigVertice();
-                }
-            }
-            return null;
-    }
-    
-    public void insertarArista(Vertice origen,Vertice destino){
-        if(origen==null || destino ==null){
-            System.out.println("No se puede papu");
-        }else{
-            Arista nuevaArista = new Arista(destino);
-            if(origen.getInicioArista()==null){
-                origen.setInicioArista(nuevaArista);
-            }else{
-            Arista actual = origen.getInicioArista();
-            while(actual.getSigArista() != null){
-                        actual = actual.getSigArista();
-            }
-            actual.setSigArista(nuevaArista);
-        }
-      }
-
-    }
-    public void mostrarListaAdyacencia (){
-        Vertice actual = this.getInicio();
-        while (actual != null){
-            System.out.print(actual.getDato() + ": ");
-            Arista actualArista = actual.getInicioArista();
-            while(actualArista != null){
-                System.out.print(actualArista.getVerticeDestino().getDato()+"->");
-                actualArista = actualArista.getSigArista();
-            }
-            System.out.println("");
-            actual = actual.getSigVertice();
-        }
-        
-        
-    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int opcion;
+        String dirIP, nombre, tipo;
+        float capacidad, ponderacion;
+        Dispositivo dispositivoOrigen, dispositivoDestino;
+        RedTelecom redTelecom = new RedTelecom();
+        Scanner scanner = new Scanner(System.in);
+
+        do{
+            System.out.println("1. Agregar dispositivo");
+            System.out.println("2. Agregar arista");
+            System.out.println("3. Mostrar lista de adyacencia");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese su opcion: ");
+            opcion = scanner.nextInt();
+            switch(opcion){
+                case 1:
+                    System.out.print("Ingrese la dirección IP del dispositivo: ");
+                    dirIP = scanner.next();
+                    System.out.print("Ingrese el nombre del dispositivo: ");
+                    nombre = scanner.next();
+                    System.out.print("Ingrese el tipo del dispositivo: ");
+                    tipo = scanner.next();
+                    System.out.print("Ingrese la capacidad del dispositivo: ");
+                    capacidad = scanner.nextFloat();
+                    redTelecom.agregarDispositivo(dirIP, nombre, tipo, capacidad);
+                    break;
+                case 2:
+                    System.out.print("Ingrese la dirección IP del dispositivo origen: ");
+                    dirIP = scanner.next();
+                    dispositivoOrigen = redTelecom.buscarDispositivo(dirIP);
+                    System.out.print("Ingrese la dirección IP del dispositivo destino: ");
+                    dirIP = scanner.next();
+                    dispositivoDestino = redTelecom.buscarDispositivo(dirIP);
+                    System.out.print("Ingrese la ponderación de la arista: ");
+                    ponderacion = scanner.nextFloat();
+                    redTelecom.agregarArista(dispositivoOrigen, dispositivoDestino, ponderacion);
+                    break;
+                case 3:
+                    redTelecom.mostrarListaAdyacencia();
+                    break;
+                case 4:
+                    System.out.println("Saliendo...");
+                    scanner.close();
+                    break;
+                default:
+                    System.out.println("Opción inválida");
+            }
+        }while(opcion != 4);
     }
 }
