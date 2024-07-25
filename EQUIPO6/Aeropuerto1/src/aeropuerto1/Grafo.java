e1186a7df20744355065059b67cff2508965e573
15Tano <eltano193@gmail.com>
2024-07-25 13:16:47 -0600

    Base

--- EQUIPO6/Aeropuerto1/src/aeropuerto1/Grafo.java
+++ EQUIPO6/Aeropuerto1/src/aeropuerto1/Grafo.java
@@ -12,34 +12,5 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Grafo {
-    private List<Aeropuerto> aeropuertos;
-    private List<Ruta> rutas;
 
-    public Grafo() {
-        aeropuertos = new ArrayList<>();
-        rutas = new ArrayList<>();
-    }
-
-    public void agregarAeropuerto(Aeropuerto aeropuerto) {
-        aeropuertos.add(aeropuerto);
-    }
-
-    public void agregarRuta(Ruta ruta) {
-        rutas.add(ruta);
-    }
-
-    public Aeropuerto buscarAeropuerto(String nombre) {
-        for (Aeropuerto aeropuerto : aeropuertos) {
-            if (aeropuerto.getNombre().equals(nombre)) {
-                return aeropuerto;
-            }
-        }
-        return null;
-    }
-
-    public void mostrarListaAdyacencia() {
-        for (Ruta ruta : rutas) {
-            System.out.println(ruta);
-        }
-    }
 }
